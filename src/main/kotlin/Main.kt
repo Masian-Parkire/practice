0b06f08695fd72b57a041b790d3080ec14740acf
Masian Parkire <parkiremasian@gmail.com>
2023-02-24 16:24:45 +0300

    practice

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+var friends = arrayOf("Abel","Betty","Chris", "David" ,"Annabel",)
+  val numbers = arrayOf(32,5,67,242,90,423,211)
+   //println(friends.sortedArray().contentToString()) // creates a copy of sorted names and prints out
+
+    //println(friends.contentToString())
+    println(friends.sort()) // sorts in place and does not print anything
+
+
+
+}
